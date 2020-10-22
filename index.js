36a6233953cbf996cd76dad2a57aed7b313edfd1
mat <lishay.matvey20001506matlish>
2020-10-22 17:19:28 +0300

    (Project) Combining Reducers

--- index.js
+++ index.js
@@ -13,6 +13,24 @@ function todos(state = [], action) {
     }
 }
 
+function goals(state = [], action) {
+    switch (action.type) {
+        case 'ADD_GOAL':
+            return state.concat([action.goal])
+        case 'REMOVE_GOAL':
+            return state.filter((goal) => goal.id !== action.id)
+        default :
+            return state
+    }
+}
+
+function app (state = {}, action) {
+    return {
+        todos: todos(state.todos, action),
+        goals: goals(state.goals, action)
+    }
+}
+
 function createStore(reducer) {
     let state
     let listeners = []
@@ -38,7 +56,8 @@ function createStore(reducer) {
     }
 }
 
-const store = createStore(todos)
+
+const store = createStore(app)
 store.subscribe(() => {
     console.log('The new state is: ', store.getState())
 })
